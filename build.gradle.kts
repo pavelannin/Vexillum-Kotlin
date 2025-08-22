5c7c5204f5c3c5efdec38375ddfa6f8acf7adff3
Pavel Annin <anninpavel2014@gmail.com>
2025-08-22 07:45:29 +0300

    upgrade deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,5 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(deps.plugins.android.application) apply false
+    alias(deps.plugins.kotlin.multiplatform) apply false
+    alias(deps.plugins.kotlin.serialization) apply false
     alias(deps.plugins.android.library) apply false
-    alias(deps.plugins.kotlin.android) apply false
-    alias(deps.plugins.kotlin.jvm) apply false
-    alias(deps.plugins.publish) apply true
-    alias(deps.plugins.publish.gradle.plugin) apply false
-}
-true // Needed to make the Suppress annotation work for the plugins block
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            stagingProfileId.set(property("sonatypeStagingProfileId").toString())
-            username.set(project.property("ossrhUsername").toString())
-            password.set(project.property("ossrhPassword").toString())
-        }
-    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,34 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 pluginManagement {
     repositories {
-        gradlePluginPortal()
-        google()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
+        gradlePluginPortal()
     }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 @Suppress(names = ["UnstableApiUsage"])
 dependencyResolutionManagement {
     versionCatalogs {
         create("deps") {
-            from(files("dependencies.versions.toml"))
+            from(files("deps.versions.toml"))
         }
     }
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
+
 rootProject.name = "Vexillum"
-include(
-    ":vexillum",
-    ":gradle-plugin"
-)
