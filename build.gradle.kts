eb0a350cbf30ce93979ba1dade7741004401cd9c
Pavel Annin <anninpavel2014@gmail.com>
2025-11-12 23:04:39 +0300

    apply projects plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     alias(deps.plugins.kotlin.multiplatform) apply false
+    alias(deps.plugins.kotlin.jvm) apply false
     alias(deps.plugins.kotlin.serialization) apply false
     alias(deps.plugins.android.library) apply false
+    alias (deps.plugins.mokkery) apply false
+    alias (deps.plugins.gradle.publish) apply false
 }
